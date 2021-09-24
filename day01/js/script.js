7c49af9bb94d1d122a87b41323227358345d1869
vinsonf <vfernandez@shift3tech.com>
2021-09-23 17:40:08 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,45 @@
+// const appName = "My Cool App";
+// // const AppName = "My Cool App";
+// console.log(appName);
+
+// const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, true, false, 'a', 'b', 'c', [0, 1]];
+// const object = {
+//     name: "Brittney",
+//     isInstructor: true,
+//     score: 100,
+//     hairColor: '',
+//     favoriteFoods: [],
+//     friends: [{name: ''}]
+// }
+// const people = [
+//     object,
+//     {
+//         name: "Vinson",
+//         isInstructor: true,
+//         score: 90,
+//         hairColor: 'Brown',
+//         favoriteFoods: [],
+//         friends: []
+//     }
+
+// ];
+
+// console.log(people);
+
+// const h1 = document.querySelector('#title');
+
+// h1.addEventListener('mouseenter', function() {
+
+//     h1.style.color = 'red';
+//     h1.style.backgroundColor = 'black';
+//     h1.style.padding = '25px';
+//     h1.innerHTML = `<p> ${people[0].name} </p>`;
+// });
+// h1.addEventListener('mouseleave', function() {
+
+//     h1.style.color = 'green';
+//     h1.style.backgroundColor = 'yellow';
+//     h1.style.padding = '25px';
+//     h1.innerHTML = `<p> ${people[1].name} </p>`;
+// });
+// console.log(h1);
