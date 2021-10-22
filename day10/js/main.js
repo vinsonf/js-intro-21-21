f9f99fd6992ada60e531129e18e0afac87c499f1
vinsonf <vfernandez@shift3tech.com>
2021-10-21 21:16:15 -0700

    feat(main) letters

--- day10/js/main.js
+++ day10/js/main.js
@@ -1,7 +1,5 @@
 const numbers = [0,1,2,3,4,5,6,7,8,9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
-
-
 let health = 100;
 const myNumbers = [];
 
@@ -9,12 +7,9 @@ setInterval(() => {
     myNumbers.push(new MyNumber());
 }, 4000)
 
-
-
 function getRandomNumber() {
     return Math.floor(Math.random() * numbers.length);
 }
-
 class MyNumber{
     element = null;
     value = null;
@@ -27,26 +22,24 @@ class MyNumber{
         document.body.appendChild(this.element);
         this.element.style.position = 'absolute';
         this.id = setInterval(()=> {
-
             this.moveDown();
             if (this.start > 200) {
                 health-= 10;
                 console.log(health)
                 clearInterval(this.id);
             } 
-
         }, 1000);
     }
 
     moveDown() {
         this.start += 20;
-        console.log('second', this.start)
-        this.element.style.top = this.start + 'px'
+        console.log('second', this.start);
+        this.element.style.top = this.start + 'px';
     }
 
     test(value) {
         if (this.value === value) {
-            console.log('you got it', this.value)
+            console.log('you got it', this.value);
             clearInterval(this.id);
             this.element.remove();
         }
@@ -56,8 +49,8 @@ class MyNumber{
 new MyNumber();
 
 document.addEventListener('keydown', (event)=> {
-    console.log(event.key)
+    console.log(event.key);
     myNumbers.forEach(mn => {
-        mn.test(String(event.key))
+        mn.test(String(event.key));
     });
 })
